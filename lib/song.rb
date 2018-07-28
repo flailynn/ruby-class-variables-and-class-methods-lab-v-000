bb73c588ac6c6a1e89aacf646d7bc8c244878ffc
Anita Beauchamp <amadamspr@gmail.com>
2018-07-28 04:45:31 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,15 @@
 class Song
   attr_accessor :name, :artist, :genre
 
+  #@@count will hold the number of songs created
+  @@count = 0
+
   def initialize(name, artist, genre)
     self.name = name
     self.artist = artist
     self.genre = genre
+
+    @@count =+ 1
   end
 
 end
